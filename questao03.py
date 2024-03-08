29bebf0d5a3bca5bb11b5b4c6293ef4cb4336ba4
Laércio Fernandes Correia de Melo Neto <laerciofernandesmelonetoo@gmail.com>
2024-03-08 17:31:00 -0300

    cria sequência c, d, e e imprimindo os próximos elementos das sequências

--- questao03.py
+++ questao03.py
@@ -17,4 +17,26 @@ def proximo_elemento_b():
     proximo_numero = ultimo_numero * 2
     return proximo_numero
 
+def proximo_elemento_c():
+    ultimo_numero = 36
+    proximo_numero = (int(ultimo_numero ** 0.5) + 1) ** 2
+    return proximo_numero
 
+def proximo_elemento_d():
+    ultimo_numero = 64
+    proximo_numero = (int(ultimo_numero ** 0.5) + 1) ** 2
+    return proximo_numero
+
+def proximo_elemento_e():
+    penultimo_numero = 5
+    ultimo_numero = 8
+    proximo_numero = penultimo_numero + ultimo_numero
+    return proximo_numero
+
+print("Próximos elementos das sequências:")
+print("a) ", proximo_elemento_a())
+print("b) ", proximo_elemento_b())
+print("c) ", proximo_elemento_c())
+print("d) ", proximo_elemento_d())
+print("e) ", proximo_elemento_e())
+print("f) Indeterminado (necessita mais informações)")
