8f4595b972b9d25cacea405cb653880f3fdfdaf4
Laércio Fernandes Correia de Melo Neto <laerciofernandesmelonetoo@gmail.com>
2024-03-08 17:12:53 -0300

    recebe número a ser verificado e verifica e exibe a mensagem

--- questão02.py
+++ questão02.py
@@ -19,3 +19,9 @@ def verifica_fibonacci(numero):
 
     return False
 
+numero_verificar = int(input("Informe um número para verificar se pertence à sequência de Fibonacci: "))
+
+if verifica_fibonacci(numero_verificar):
+    print(f"O número {numero_verificar} pertence à sequência de Fibonacci.")
+else:
+    print(f"O número {numero_verificar} não pertence à sequência de Fibonacci.")
